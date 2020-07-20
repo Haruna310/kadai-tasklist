d47df808cf3386865349a07e8098412f963d4d1f
haruna.ishii <ih95310@yahoo.co,jp>
2020-07-20 03:07:34 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,57 @@
 class TasksController < ApplicationController
     def index
         @tasks = Task.all
+    end
+    
+    def show
+        @tasks = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def show
+        @task = Task.find(params[:id])
+    end
+    
+    def create
+        @task = Task.create(task_params)
+        if @task.save
+            flash[:success] = '投稿されました'
+        redirect_to tasks_path
+    else 
+        flash.now[:danger] = '正常に投稿されませんでした'
+        render :new
+    end
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        
+        if @task.update(task_params)
+            flash[:success] = '更新されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = '更新されませんでした'
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = '削除されました'
+        redirect_to tasks_url
+    end
+    
+    private
+    def task_params
+        params.require(:task).permit(:title)
+    end
 end
